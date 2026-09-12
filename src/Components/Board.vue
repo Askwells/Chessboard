5b2e0859f9b5cf23322439a0e91d9a48295257c0
CopiusInhalus <brandongilestrada@gmail.com>
2026-09-12 22:34:05 +0800

    Total Proprietary Death 2.0

--- src/Components/Board.vue
+++ src/Components/Board.vue
@@ -135,7 +135,7 @@ state.value = loadFEN();
 
 const getPieceImgURL = (piece: c.Piece) => {
   return new URL(
-    `../Assets/Images/${utils.getPieceColor(piece) === c.Color.white ? 'w' : 'b'}${utils.pieceTypeToChar(utils.getPieceType(piece))}.png`,
+    `../Assets/Images/${utils.getPieceColor(piece) === c.Color.white ? 'w' : 'b'}${utils.pieceTypeToChar(utils.getPieceType(piece))}.svg`,
     import.meta.url,
   ).href;
 };
